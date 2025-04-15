86bc9e98f22b85f0de9684fb9bc441994dd402e8
Dmoccia93 <dmoccia93@gmail.com>
2025-04-15 17:16:14 +0200

    banner width

--- src/components/OptionCard.tsx
+++ src/components/OptionCard.tsx
@@ -69,7 +69,7 @@ const OptionCard = ({
               }`}
             />
             {title.includes("weekly") && (
-              <div className="absolute top-2 left-1/2 transform -translate-x-1/2 bg-red-500 text-white py-1 px-3 text-xs font-bold">
+              <div className="absolute top-0 left-0 w-full bg-red-500 text-white py-2 text-center text-xs font-bold">
                 COMING SOON
               </div>
             )}
